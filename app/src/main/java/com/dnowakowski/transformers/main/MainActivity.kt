27a795ead5a4db64f79d11d6553af76165166e29
Damian Nowakowski <contact@dnowakowski.com>
2018-08-12 18:01:23 -0700

    Added tab layout structure

--- app/src/main/java/com/dnowakowski/transformers/main/MainActivity.kt
+++ app/src/main/java/com/dnowakowski/transformers/main/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.dnowakowski.transformers.main
 
+import android.content.Context
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
 import com.dnowakowski.transformers.R
 import com.dnowakowski.transformers.base.BaseActivity
 import com.dnowakowski.transformers.databinding.ActivityMainBinding
 import com.dnowakowski.transformers.injection.activity.ActivityComponent
+import com.dnowakowski.transformers.main.autobotTab.AutobotFragment
+import com.dnowakowski.transformers.main.deceptionTab.DeceptionFragment
+import com.dnowakowski.transformers.main.tournamentTab.TournamentFragment
 import io.reactivex.disposables.CompositeDisposable
 import javax.inject.Inject
 
@@ -20,6 +27,9 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun provideLayout(): Int = R.layout.activity_main
 
     override fun bindData(binding: ActivityMainBinding) {
+        binding.viewModel = viewModel
+        binding.viewPager.adapter = FragmentPageAdapter(this,supportFragmentManager)
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
     }
 
     override fun subscribeViewModel(): CompositeDisposable? {
@@ -36,6 +46,24 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     private fun handleViewModelEvents(event: MainEvent) {
 
     }
-
-
 }
+
+
+class FragmentPageAdapter(private val context: Context, fm: FragmentManager): FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        return when(position) {
+            0 -> AutobotFragment()
+            1 -> DeceptionFragment()
+            2 -> TournamentFragment()
+            else -> throw IndexOutOfBoundsException()
+        }
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? =
+            context.resources.getStringArray(R.array.tab_names)[position]
+
+
+    override fun getCount(): Int = 3
+}
+
